de3068579e3230fc0423621509713c4a8f5cae3a
Bijan Jadhavji <12b.jad@gmail.com>
2025-10-01 14:18:13 -0500

    updated state

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+/**
+ * @typedef Player
+ * @property {number} id
+ * @property {string} name
+ * @property {string} breed
+ * @property {string} status
+ * @property {string} imageUrl
+ * @property {string} teamId
+ */
+
 // === Constants ===
 const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
-const API = BASE + COHORT;
+const COHORT = "/2508-BIJAN";
+const RESOURCE = "/players";
+const API = BASE + COHORT + RESOURCE;
+
+// === STATE ===
+
+let players = [];
+let selectedPlayer;
