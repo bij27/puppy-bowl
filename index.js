8ca01c99c5521ee07dc3d30a746676d19dd7ebc2
Bijan Jadhavji <12b.jad@gmail.com>
2025-10-01 14:18:58 -0500

    updated get player functions for all players and single players

--- index.js
+++ index.js
@@ -18,3 +18,29 @@ const API = BASE + COHORT + RESOURCE;
 
 let players = [];
 let selectedPlayer;
+
+// == Updates state with all players from API ==
+
+async function getPlayers() {
+  try {
+    const res = await fetch(API);
+    const result = await res.json();
+    players = result.data.players;
+    render();
+  } catch (err) {
+    console.error("Error fetching parties:", err);
+  }
+}
+
+// == Updates state with a single player from the API
+
+async function getPlayer(id) {
+  try {
+    const response = await fetch(API + "/" + id);
+    const result = await response.json();
+    selectedPlayer = result.data.player;
+    render();
+  } catch (err) {
+    console.error(err);
+  }
+}
