539b6d6459256de3ecb1c97895417b5c218e4865
Bijan Jadhavji <12b.jad@gmail.com>
2025-10-01 14:20:24 -0500

    added list of all players + detailed information about selected player

--- index.js
+++ index.js
@@ -44,3 +44,42 @@ async function getPlayer(id) {
     console.error(err);
   }
 }
+
+// === Components ===
+
+// == Player name that shows more details when clicked ==
+function PlayerListItem(player) {
+  const $li = document.createElement("li");
+  $li.innerHTML = `
+  <a href="#selected">${player.name}</a>
+  `;
+  $li.addEventListener("click", () => getPlayer(player.id));
+  return $li;
+}
+
+/** A list of names of all players */
+function PlayerList() {
+  const $ul = document.createElement("ul");
+  $ul.classList.add("players");
+  const $players = players.map(PlayerListItem);
+  $ul.replaceChildren(...$players);
+  return $ul;
+}
+
+/** Detailed information about the selected player */
+function PlayerDetails() {
+  if (!selectedPlayer) {
+    const $p = document.createElement("p");
+    $p.textContent = "Please select a pup to learn more!";
+    return $p;
+  }
+  const $section = document.createElement("section");
+  $section.classList.add("player");
+  $section.innerHTML = `
+  <figure>
+  <img alt="${selectedPlayer.teamId}" src="${selectedPlayer.imageUrl}"
+  </figure>
+  <p><strong>Name:</strong> ${selectedPlayer.name}</p>
+  <p><strong>Breed:</strong> ${selectedPlayer.breed}</p>
+  <p><strong>Status:</strong> ${selectedPlayer.status}</p>
+  `;
